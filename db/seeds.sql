3e9e4fd9d30753a0d87f61d5188d21968a534f36
Juho H <juho.hautala@helsinki.fi>
2014-03-23 19:09:44 +0200

    seed user data

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (username, email, password_hash, password_salt) VALUES
+('testos', 'testos@teroni.fi', '120938lasdkjf23', '1029flsadk3');
+
+INSERT INTO users (username, email, password_hash, password_salt) VALUES
+('juhoh', 'juhoh@teroni.fi', 'ae120938lasdkjf23', 'ladf1029flsadk3');
+
