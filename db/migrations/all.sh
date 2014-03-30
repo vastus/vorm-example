2ab70d71f8566c175b5108cad41965b101a4961b
Juho H <juho.hautala@helsinki.fi>
2014-03-30 22:11:36 +0300

    changed db name

--- db/migrations/all.sh
+++ db/migrations/all.sh
@@ -1,7 +1,7 @@
 set -e
 
 MYSQL_USER=testos
-MYSQL_DBNAME=vorm
+MYSQL_DBNAME=vorm_development
 
 for relpath in $(ls ../sql/*.sql); do
   mysql -u$MYSQL_USER --password=$MYSQL_PASSWD $MYSQL_DBNAME < $relpath
