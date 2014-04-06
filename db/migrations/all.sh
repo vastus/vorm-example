e765e6124d15957251f652e33a58b04fc5153689
Juho H <juho.hautala@helsinki.fi>
2014-04-06 18:17:07 +0300

    tweaking migrations script

--- db/migrations/all.sh
+++ db/migrations/all.sh
@@ -1,9 +1,12 @@
 set -e
 
 MYSQL_USER=testos
-MYSQL_DBNAME=vorm_development
 
 for relpath in $(ls ../sql/*.sql); do
-  mysql -u$MYSQL_USER --password=$MYSQL_PASSWD $MYSQL_DBNAME < $relpath
+  mysql -u$MYSQL_USER --password=$MYSQL_PASS vorm_development < $relpath
+done
+
+for relpath in $(ls ../sql/*.sql); do
+  mysql -u$MYSQL_USER --password=$MYSQL_PASS vorm_test < $relpath
 done
 
