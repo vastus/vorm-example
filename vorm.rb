1922e801ca9462d90a8165b7e5721cbfbf82413c
Juho H <juho.hautala@helsinki.fi>
2014-04-06 22:32:56 +0300

    require helpers and enamble method overriding

--- vorm.rb
+++ vorm.rb
@@ -1,6 +1,7 @@
 require 'sinatra/base'
 
 #require './config'
+require './models/concerns/helpers'
 require './controllers/users_controller'
 require './controllers/sessions_controller'
 require './controllers/categories_controller'
@@ -9,6 +10,7 @@ Slim::Engine.set_default_options(:pretty => true, :sort_attrs => false)
 
 class Vorm < Sinatra::Base
   enable :sessions
+  enable :method_override
   use UsersController
   use SessionsController
   use CategoriesController
