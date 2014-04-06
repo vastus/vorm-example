898271df0b1fcbe56726fcb42e54b71b816c629e
Juho H <juho.hautala@helsinki.fi>
2014-04-06 18:18:39 +0300

    use categories ctrlr in app + set slim output to pretty

--- vorm.rb
+++ vorm.rb
@@ -3,10 +3,14 @@ require 'sinatra/base'
 #require './config'
 require './controllers/users_controller'
 require './controllers/sessions_controller'
+require './controllers/categories_controller'
+
+Slim::Engine.set_default_options(:pretty => true, :sort_attrs => false)
 
 class Vorm < Sinatra::Base
   enable :sessions
   use UsersController
   use SessionsController
+  use CategoriesController
 end
 
