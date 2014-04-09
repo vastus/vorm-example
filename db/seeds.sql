f59d506ac3d8f27b469101d462a9b72a13c6f9ae
Juho H <juho.hautala@helsinki.fi>
2014-04-10 00:58:57 +0300

    no need for sql seeds, use rb instead

--- db/seeds.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO users (username, email, password_hash, password_salt) VALUES
-('testos', 'testos@teroni.fi', '120938lasdkjf23', '1029flsadk3');
-
-INSERT INTO users (username, email, password_hash, password_salt) VALUES
-('juhoh', 'juhoh@teroni.fi', 'ae120938lasdkjf23', 'ladf1029flsadk3');
-
