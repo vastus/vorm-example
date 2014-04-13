bddc4e1e3b125906ce72ac9a27b5ed21b29d7a6e
Juho H <juho.hautala@helsinki.fi>
2014-04-13 21:22:24 +0300

    fix path where sql files are

--- db/run_migrations.sh
+++ db/run_migrations.sh
@@ -2,11 +2,11 @@ set -e
 
 MYSQL_USER=testos
 
-for relpath in $(ls ../sql/*.sql); do
+for relpath in $(ls sql/*.sql); do
   mysql -u$MYSQL_USER --password=$MYSQL_PASS vorm_development < $relpath
 done
 
-for relpath in $(ls ../sql/*.sql); do
+for relpath in $(ls sql/*.sql); do
   mysql -u$MYSQL_USER --password=$MYSQL_PASS vorm_test < $relpath
 done
 
