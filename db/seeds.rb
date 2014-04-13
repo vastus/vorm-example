d61a1a2d36f5e7a906e613b68bc582a30ff0e174
Juho H <juho.hautala@helsinki.fi>
2014-04-13 21:22:59 +0300

    use of new .create method + topic seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,12 +2,17 @@ ENV['RACK_ENV'] ||= 'development'
 
 require_relative '../models/user'
 require_relative '../models/category'
+require_relative '../models/topic'
 
 User.destroy_all
-User.new(username: 'testos', email: 'testos@teroni.fi', password: 'secretos', password_confirmation: 'secretos').save
-User.new(username: 'juhoh', email: 'juhoh@lawdy.fi', password: 'juhoh', password_confirmation: 'juhoh').save
+testos = User.create(username: 'testos', email: 'testos@teroni.fi', password: 'secretos', password_confirmation: 'secretos')
+juhoh = User.create(username: 'juhoh', email: 'juhoh@lawdy.fi', password: 'juhoh', password_confirmation: 'juhoh')
 
 Category.destroy_all
-Category.new(name: 'General', description: 'General discussions. Can be (almost) anything.').save
-Category.new(name: 'Programming', description: 'Datatypes, algorithms, languages, and everything else.').save
+gen = Category.create(name: 'General', description: 'General discussions. Can be (almost) anything.')
+prog = Category.create(name: 'Programming', description: 'Datatypes, algorithms, languages, and everything else.')
+street = Category.create(name: 'Cholo', description: 'Street man is the man. Hobos, hobas and such.')
+
+Topic.destroy_all
+Topic.create(title: 'Present yourself', body: 'Everybody say my name!', category_id: gen.id)
 
