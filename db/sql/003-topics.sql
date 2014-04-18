bee5fdd38c4e66c8006e54e09a78c5d8f2f4659a
Juho H <juho.hautala@helsinki.fi>
2014-04-18 20:03:08 +0300

    add user_id to topics table

--- db/sql/003-topics.sql
+++ db/sql/003-topics.sql
@@ -2,13 +2,18 @@ CREATE TABLE topics (
   id INT NOT NULL AUTO_INCREMENT,
   title VARCHAR(32) NOT NULL,
   body TEXT NOT NULL,
+  -- timestamp DATETIME NOT NULL,
   category_id INT NOT NULL,
+  user_id INT NOT NULL,
 
-  PRIMARY KEY (id),
-  INDEX (category_id),
-  FOREIGN KEY (category_id) REFERENCES categories(id)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE
+  PRIMARY KEY (id)
+  -- INDEX (category_id),
+  -- FOREIGN KEY (category_id) REFERENCES categories(id)
+  --   ON DELETE CASCADE
+  --   ON UPDATE CASCADE,
+  -- FOREIGN KEY (user_id) REFERENCES user(id)
+  --   ON DELETE CASCADE
+  --   ON UPDATE CASCADE
     -- ref_opt: RESTRICT | CASCADE | SET NULL | NO ACTION
 );
 
