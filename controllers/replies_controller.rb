038d76a67cfacc32eb2b8e5761a0ebc31345c474
Juho H <juho.hautala@helsinki.fi>
2014-04-27 22:43:05 +0300

    authenticate user before creating reply

--- controllers/replies_controller.rb
+++ controllers/replies_controller.rb
@@ -1,22 +1,12 @@
-require 'sinatra/base'
-require 'slim'
-
-class RepliesController < Sinatra::Base
-  set :views, 'views'
-
+class RepliesController < AppController
   # Create.
   post '/topics/:topic_id/replies/new' do
+    authenticate!
     @topic = Topic.find(params[:topic_id])
     @reply = Reply.new(params[:reply])
     if @reply.save
       redirect to(url("/topics/#{@topic.id}"))
     else
-      puts params[:reply]
-      puts @reply.id
-      puts @reply.message
-      puts @reply.user_id
-      puts @reply.topic_id
-      puts @reply
       slim :'topics/show'
     end
   end
