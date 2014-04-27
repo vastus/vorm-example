f69611a026ac499541e99e32e050ebb3a8c8265e
Juho H <juho.hautala@helsinki.fi>
2014-04-27 22:44:13 +0300

    authenticate actions and unread route

--- controllers/topics_controller.rb
+++ controllers/topics_controller.rb
@@ -1,32 +1,40 @@
-# TODO: move these to some more general place from every model
-require 'sinatra/base'
-require 'slim'
-
-# require_relative '../models/category'
-
-class TopicsController < Sinatra::Base
-  set :views, 'views'
-  # helpers Helpers
-
+class TopicsController < AppController
   # Show.
   get '/topics/:id' do
     @topic = Topic.find(params[:id])
+    mark_as_read!(@topic)
     slim :'topics/show'
   end
 
   # New.
   get '/categories/:category_id/topics/new' do
+    authenticate!
     @topic = Topic.new
     slim :'topics/new'
   end
 
   # Create.
   post '/categories/:category_id/topics' do
+    authenticate!
     @topic = Topic.new(params[:topic])
     if @topic.save
       redirect to(url("/topics/#{@topic.id}"))
     else
       slim :'topics/new'
+    end
+  end
+
+  # Unread.
+  get '/unread' do
+    authenticate!
+    @topics = current_user.unread
+    slim :'topics/index'
+  end
+
+  private 
+  def mark_as_read!(topic)
+    if current_user
+      ReadTopics.create(user_id: current_user.id, topic_id: topic.id)
     end
   end
 end
