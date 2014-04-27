2122e0b0a9b407da49bb51c213ae4705159ce513
Juho H <juho.hautala@helsinki.fi>
2014-04-27 22:49:17 +0300

    add check ownership to helpers and implement admin? helper method

--- models/concerns/helpers.rb
+++ models/concerns/helpers.rb
@@ -3,9 +3,14 @@ module Helpers
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
+  def check_ownership!
+    if params[:id] != session[:user_id].to_s
+      redirect to(url("/login"))
+    end
+  end
+
   def admin?
-    # current_user && current_user.admin?
-    true
+    current_user && current_user.role?(:admin)
   end
 end
 
